595716bf74882d2f3b1d22937d98d47d1df42c0d
genii grace <jeyeom10@naver.com>
2023-01-14 02:55:46 +0900

    공지사항 게시판 내 등록일자 연/월/일로 표기하기 : LocalDateTime regTime으로는 진행이 어려워 따로  String crateTime 으로 작성하여 추가함

--- 230112_shop-master/src/main/java/com/shop/entity/BaseTimeEntity.java
+++ 230112_shop-master/src/main/java/com/shop/entity/BaseTimeEntity.java
@@ -9,18 +9,31 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
-@Getter @Setter
+@Getter
+@Setter
 public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime regTime;
 
+    @CreatedDate
+    @Column(updatable = false)
+    private String createTime;
+
     @LastModifiedDate
-    private LocalDateTime updateTime;
+    private String updateTime;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.updateTime = this.createTime;
+    }
 
 }
