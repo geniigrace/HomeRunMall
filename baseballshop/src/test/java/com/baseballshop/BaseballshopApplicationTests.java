412939a030a9f54451f3835752ab9791cc68e703
geniigrace <jeyeom10@naver.com>
2023-01-20 01:23:48 +0900

    BBShop Start!

---
+++ baseballshop/src/test/java/com/baseballshop/BaseballshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseballshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
