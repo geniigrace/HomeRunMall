fb372e9349b5b39af2d30981f80fc7882fbe24a6
geniigrace <jeyeom10@naver.com>
2023-01-27 02:21:56 +0900

    Enum 을 String 값으로 대신하는 것으로 수정

--- baseballshop/src/main/java/com/baseballshop/constant/Category.java
+++
@@ -1,7 +0,0 @@
-package com.baseballshop.constant;
-
-public enum Category {
-
-    UNIFORM, BALLCAP, CLOTHES, GOODS, BBITEM, CHEERITEM, PLAYER, EVENT
-
-}
--- baseballshop/src/main/java/com/baseballshop/constant/Team.java
+++
@@ -1,6 +0,0 @@
-package com.baseballshop.constant;
-
-public enum Team {
-
-    KOREA, DS, SSG, KW, LG, KT, KIA, NC, SS, LT, HH
-}
