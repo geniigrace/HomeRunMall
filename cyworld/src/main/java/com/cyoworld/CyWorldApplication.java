f980268ddf9b4ca79c190590323b9fbe0b3b3542
geniigrace <jeyeom10@naver.com>
2023-02-09 14:01:42 +0900

    경로수정

---
+++ cyworld/src/main/java/com/cyoworld/CyWorldApplication.java
@@ -0,0 +1,13 @@
+package com.cyoworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyWorldApplication.class, args);
+	}
+
+}
