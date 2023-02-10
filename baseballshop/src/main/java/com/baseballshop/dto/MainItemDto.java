2e57e109d5c3c3bd036bbb1842dd55bc31c52a5f
geniigrace <jeyeom10@naver.com>
2023-02-11 05:01:16 +0900

    메인 슬라이드 완료 / 상품 리스트 추가 구성해야함

---
+++ baseballshop/src/main/java/com/baseballshop/dto/MainItemDto.java
@@ -0,0 +1,31 @@
+package com.baseballshop.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainItemDto {
+
+    private Long id;
+    private String itemName;
+
+    private String itemDetail;
+
+    private String imgUrl;
+
+    private Integer price;
+
+    @QueryProjection // QureyDSL 조회시 MainItemDto 객체로 바로 오도록 활용 : DTO가 QDTO로 나올 수 있음
+    public MainItemDto(Long id, String itemName, String itemDetail, String imgUrl, Integer price){
+
+        this.id=id;
+        this.itemName = itemName;
+        this.itemDetail = itemDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+
+    }
+
+}
--- baseballshop/src/main/java/com/baseballshop/repository/ItemRepositoryCustom.java
+++ baseballshop/src/main/java/com/baseballshop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.baseballshop.repository;
 
 import com.baseballshop.dto.ItemSearchDto;
+import com.baseballshop.dto.MainItemDto;
 import com.baseballshop.entity.Item;
 
 import org.springframework.data.domain.Page;
@@ -10,5 +11,8 @@ public interface ItemRepositoryCustom {
 
     //상품관리페이지
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 
 }
